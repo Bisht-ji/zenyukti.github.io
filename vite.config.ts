21a94c7988e8ffb676e83d81ae2493050bff439a
Aditya bisht <adityabisht.cx@gmail.com>
2025-09-06 12:48:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import VitePluginSitemap from "vite-plugin-pages-sitemap";
-import { format } from "prettier"; // optional, for XML formatting
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     react(),
-    VitePluginSitemap({
-      hostname: "https://zenyukti.in",
-      routes: [
-        { path: "/", changefreq: "daily", priority: 1.0 },
-        { path: "/about", changefreq: "monthly", priority: 0.8 },
-        { path: "/community", changefreq: "weekly", priority: 0.7 },
-        { path: "/contact", changefreq: "monthly", priority: 0.6 },
-        { path: "/join-us", changefreq: "weekly", priority: 0.7 },
-        { path: "/team", changefreq: "monthly", priority: 0.8 },
-      ],
-      // pretty format XML
-      xmlFormatter: (xml: string) => format(xml, { parser: "html" }),
-    }),
-  ],
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
